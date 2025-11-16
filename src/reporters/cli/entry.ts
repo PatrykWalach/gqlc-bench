6acd4ed24f062ada9d72ec24f40d26104a613e12
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-11-16 02:06:07 +0100

    fix cli

--- src/reporters/cli/entry.ts
+++ src/reporters/cli/entry.ts
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
-import { h } from 'ink';
-
-import { polyfillEnvironment } from '../../environment';
+import { h } from "ink"
+import { main } from "./main"
+import { polyfillEnvironment } from "../../environment"
 
 // So that we can load examples, benchmarks, etc.
-polyfillEnvironment();
+polyfillEnvironment()
 // So that we can compile all JSX logic for Ink, and for React, with the same
 // TypeScript configuration.
-(global as any).React = { createElement: h };
+;(global as any).React = { createElement: h }
 
 // Aaaaand go (lazily).
-require('./main')
-  .main()
-  .catch(error => process.stderr.write(error.stack));
+
+main().catch((error) => process.stderr.write(error.stack))
